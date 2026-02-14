b25c5d14f1867c050a929fc42ca319ce345dee87
INTEL <abdirizackibrahim252@gmail.com>
2026-02-14 14:42:03 +0300

    Add files via upload

---
+++ primitive.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int integerNum = 10;
+    float floatNum = 3.14;
+    char character = 'A';
+    bool booleanValue = true;
+
+    cout << integerNum << endl;
+    cout << floatNum << endl;
+    cout << character << endl;
+    cout << booleanValue << endl;
+
+    return 0;
+}
